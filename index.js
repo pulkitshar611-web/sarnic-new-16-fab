b8fb807ce0f5f25411e13c71bc416c82e1b6d05c
GautamBairagi <gauatambairagi22@gmail.com>
2026-02-16 17:08:09 +0530

    asdasd

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/api/ics-proxy", async (req, res) => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:5173", "sarnic-latest-one.netlify.app"],
+    origin: ["http://localhost:5173", "https://sarnic-latest-one.netlify.app", "https://project.phoenix-dezign.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
