f67e3ef4b571979a912401b3636064ef10394b68
pulkitshar611-web <pulkitshar611@gmail.com>
2026-02-17 12:42:32 +0530

    Update index.js dfsfd
    sdfsdf

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get("/api/ics-proxy", async (req, res) => {
 });
 app.use(
   cors({
-    origin: ["http://localhost:5173", "sarnic-latest-one.netlify.app","https://sarnic.kiaantechnology.com"],
+    origin: ["http://localhost:5173", "https://sarnic-latest-one.netlify.app", "https://sarnic.kiaantechnology.com", "https://project.phoenix-dezign.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
